1cad92a454a68e505a1d09775f53f7a93f0af722
Bruno Carneiro <bruno@zapt.tech>
2025-06-10 16:31:48 -0300

    Handling URLs params when comming from intents

--- app/src/main/java/tech/zapt/zaptsdkreferenceapp/MapViewActivity.java
+++ app/src/main/java/tech/zapt/zaptsdkreferenceapp/MapViewActivity.java
@@ -3,18 +3,22 @@ package tech.zapt.zaptsdkreferenceapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
 import android.webkit.WebView;
 
-import java.util.Collection;
+import com.android.installreferrer.api.InstallReferrerClient;
+import com.android.installreferrer.api.InstallReferrerStateListener;
+import com.android.installreferrer.api.ReferrerDetails;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import tech.zapt.sdk.location.ZaptSDK;
-import tech.zapt.sdk.location.beacon.Beacon;
-import tech.zapt.sdk.location.beacon.BeaconListener;
 
 public class MapViewActivity extends Activity {
 
@@ -30,9 +34,9 @@ public class MapViewActivity extends Activity {
 		setContentView(R.layout.map);
 		zaptWebView = (WebView) findViewById(R.id.webView);
 		initializeZaptSDK();
-		startWebView();
-		listenBeacon();
+		configureInstallReferrer();
 	}
+
 
 	public void initializeZaptSDK() {
 		zaptSDK = ZaptSDK.getInstance(this.getApplicationContext());
@@ -48,36 +52,99 @@ public class MapViewActivity extends Activity {
 		}
 	}
 
-	public void startWebView() {
+	private void configureInstallReferrer(){
+		final InstallReferrerClient referrerClient;
+		referrerClient = InstallReferrerClient.newBuilder(this).build();
+		referrerClient.startConnection(new InstallReferrerStateListener() {
+			@Override
+			public void onInstallReferrerSetupFinished(int responseCode) {
+				switch (responseCode) {
+					case InstallReferrerClient.InstallReferrerResponse.OK:
+						ReferrerDetails response = null;
+						try {
+							response = referrerClient.getInstallReferrer();
+							String referrerUrl = response.getInstallReferrer();
+							boolean instantExperienceLaunched = response.getGooglePlayInstantParam();
+							startWebView(referrerUrl);
+						} catch (RemoteException e) {
+							startWebView(null);
+						}
+						break;
+					case InstallReferrerClient.InstallReferrerResponse.FEATURE_NOT_SUPPORTED:
+						// API not available on the current Play Store app.
+						startWebView(null);
+						break;
+					case InstallReferrerClient.InstallReferrerResponse.SERVICE_UNAVAILABLE:
+						// Connection couldn't be established.
+						startWebView(null);
+						break;
+				}
+			}
+
+			@Override
+			public void onInstallReferrerServiceDisconnected() {
+				// Try to restart the connection on the next request to
+				// Google Play by calling the startConnection() method.
+			}
+		});
+	}
+
+	public void startWebView(String paramsStr) {
 		// Add custom options in order to customize map behaviour
 		Map<String, String> opts = new HashMap<>();
 		opts.put("bottomNavigation", "false");
 
+		if(paramsStr != null) {
+			String [] params = paramsStr.split("&");
+			for(String param : params) {
+				String [] paramKeyValue = param.split("=");
+				if(!"placeId".equals(paramKeyValue[0])){
+					opts.put(paramKeyValue[0], paramKeyValue[1]);
+				}
+			}
+		}
+
+		String urlSuffix = handleIntent(getIntent());
+
 		// Get map link with the opts
 		String url = zaptSDK.getMapLink(opts);
+
+		if(urlSuffix != null && !urlSuffix.isEmpty()) {
+			if(!url.contains("?")) {
+				url+="?";
+			}
+			if(!urlSuffix.startsWith("&") && !url.endsWith("&")) {
+				urlSuffix="&" + urlSuffix;
+			}
+			url+=urlSuffix;
+		}
 
 		// init Webview with the URL
 		zaptWebView.loadUrl(url);
 	}
 
-	/**
-	 * Example of usage of listening beacons
-	 */
-	private void listenBeacon() {
-		zaptSDK.addBeaconListener(new BeaconListener() {
-			@Override
-			public void onScan(Collection<Beacon> collection) {
-				for (Beacon beacon : collection) {
-					Log.i("zapt.tech", "Beacon Found: " + beacon.getDistance());
-				}
-			}
-		});
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		String urlSuffix = handleIntent(intent);
+		zaptWebView.loadUrl(urlSuffix);
 	}
 
+	private String handleIntent(Intent intent) {
+		String appLinkAction = intent.getAction();
+		Uri appLinkData = intent.getData();
+		if (Intent.ACTION_VIEW.equals(appLinkAction) && appLinkData != null){
+			String url = appLinkData.toString()
+				.replace("https://app.zapt.tech/instant/viracopos/", "")
+				.replace("https://app.zapt.tech/instant/viracopos", "")
+				.replace("?", "");
+			return url;
+		}
+		return "";
+	}
 
 	@Override
-	public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
 		switch (requestCode) {
 			case PERMISSION_REQUEST_FINE_LOCATION: {
 				if(grantResults == null || grantResults.length == 0) {
